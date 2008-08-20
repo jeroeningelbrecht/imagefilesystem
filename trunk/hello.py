56b286b8c5db25cd143e029cebb135738362e2bb
jeroeningelbrecht <jeroeningelbrecht@a888fc79-3454-0410-b17d-bf46f5c825f5>
2008-08-20 15:50:48 +0000

    versie waarbij de structuur wordt opgebouwd, de bestanden worden gelezen en vervormd. Opgelet, met PNG bestanden geeft de display problemen 'corrupt image' maar met een gewone imageviewer geeft hij geen fouten

--- trunk/hello.py
+++ trunk/hello.py
@@ -24,7 +24,6 @@ class MyStat(fuse.Stat):
 class HelloFS(Fuse):
     
     def __init__(self,*args,**kwargs):
-    
         fuse.Fuse.__init__(self,*args,**kwargs)
         self.src = sys.argv[1] #de bronmap
         self.dest = sys.argv[2] #dest map
@@ -36,8 +35,11 @@ class HelloFS(Fuse):
         l = f.readlines()
         
         self.pathTmpDir = l[l.index("tmpdir\n")+1].strip()
-        self.width = l[l.index("width\n")+1].strip()
-        self.height = l[l.index("height\n")+1].strip()
+        self.THUMB_width = l[l.index("THUMB_width\n")+1].strip()
+        self.THUMB_height = l[l.index("THUMB_height\n")+1].strip()
+        self.SMALL_width = l[l.index("SMALL_width\n")+1].strip()
+        self.SMALL_height = l[l.index("SMALL_height\n")+1].strip()
+        self.ROTATED_angle = l[l.index("ROTATED_angle\n")+1].strip()
         
         #making the temporary dirs
         if os.path.isdir(self.pathTmpDir):
@@ -46,55 +48,105 @@ class HelloFS(Fuse):
         
         for root, dirs, files in os.walk(self.src, topdown=True):
             str = root.split(self.src)[1]
-            os.makedirs(self.pathTmpDir + str + "/THUMB")
+            for dir in ["/THUMB", "/SMALL", "/ROTATED"]:
+                os.makedirs(self.pathTmpDir + str + dir)
         
+    def getattr(self, path):
         
+        if len(path.split("/THUMB")) > 1:
+            pathSplit = path.split("/THUMB") 
+            return self.returnLstat(pathSplit)
+        
+        elif len(path.split("/SMALL")) > 1:
+            pathSplit = path.split("/SMALL")
+            return self.returnLstat(pathSplit)
+
+        elif len(path.split("/ROTATED")) > 1:
+            pathSplit = path.split("/ROTATED") 
+            return self.returnLstat(pathSplit)
+            
+        else:
+            return os.lstat(self.pathTmpDir + path)
+
+    def readdir(self, path, offset):
+        dirents = ['.', '..']        
+        
+        if len(path.split("/THUMB")) > 1:
+            pathSplit = path.split("/THUMB")
+            files = os.listdir(self.src + pathSplit[0])
+        elif len(path.split("/SMALL")) > 1:
+            pathSplit = path.split("/SMALL")
+            files = os.listdir(self.src + pathSplit[0])
+        elif len(path.split("/ROTATED")) > 1:
+            pathSplit = path.split("/ROTATED")
+            files = os.listdir(self.src + pathSplit[0])
+            
+        else:
+            files = os.listdir(self.pathTmpDir + path)
+            
+        for f in files:
+            dirents.append(f)
+        for r in  dirents:
+            yield fuse.Direntry(r)
+            
+    def open(self, path, flags): ###controle of file al bestaat, zoniet: aanmaken!
+        if not os.path.isfile(self.pathTmpDir + path):
+            
+            if len(path.split("/THUMB")) > 1:
+                command = self.getResizeCommand(path.split("/THUMB"), path)
+            
+            elif len(path.split("/SMALL")) > 1 :
+                command = self.getResizeCommand(path.split("/SMALL"), path)
+                
+            elif len(path.split("/ROTATED")) > 1 :
+                pathSplit = path.split("/ROTATED")
+                pth = self.src + pathSplit[0] + pathSplit[len(pathSplit)-1]
+                command = "convert " +pth+ " -rotate " +self.ROTATED_angle + " " +self.pathTmpDir +path
+            else:
+                pass
+            
+            accmode = os.O_RDONLY | os.O_WRONLY | os.O_RDWR
+            if (flags & accmode) != os.O_RDONLY:
+                return -errno.EACCES
+            os.popen(command, "r")
+            
+        else:
+            pass
+        
+    def read(self, path, size, offset):
+        imgFile = open(self.pathTmpDir + path, "rb")
+        imgFile.seek(offset)
+        imgBytes = imgFile.read(size)
+        self.wrt(self.pathTmpDir + path) 
+        return imgBytes
+
+    ###Logging###
     def wrt(self, str):
         f = open("/tmp/log.txt", 'a')
         f.write(str+"\n")
         f.close()
         
-    def getattr(self, path):
-        #st = MyStat()
-        #if path == '/':
-        #    st.st_mode = stat.S_IFDIR | 0755
-        #    st.st_nlink = 2
-        #else:            
-        #    st.st_mode = stat.S_IFREG | 0777
-        #    st.st_nlink = 1
-        #    st.st_size = 1024
-        #return st
+    def wrtList(self, list):
+        f = open("/tmp/log.txt", 'a')
+        for str in list:
+            f.write(str+"\n")
+        f.close()
         
-        return os.lstat(self.pathTmpDir + path)
-
-    def readdir(self, path, offset):
-        dirents = ['.', '..']
-        files = os.listdir(self.pathTmpDir + path)
-        for f in files:
-            dirents.append(f)
-        for r in  dirents:
-            yield fuse.Direntry(r)
-        
-            
-    def open(self, path, flags):
-        
-        pth = self.src + path #/home/jeroen/Desktop/test + /python.png
-        comm = "convert " +pth+ " -resize " +self.width+ "x" +self.height+ " /tmp" +path
-        
-        if not os.path.isfile(pth):
-            return -errno.ENOENT
-        accmode = os.O_RDONLY | os.O_WRONLY | os.O_RDWR
-        if (flags & accmode) != os.O_RDONLY:
-            return -errno.EACCES
-        
-        
-        #os.popen(comm, "r")
-        
-    def read(self, path, size, offset):
-        imgFile = open("/tmp" + path, "rb")
-        imgFile.seek(offset)        
-        imgBytes = imgFile.read(size)
-        return imgBytes
+    ## returning lstat info
+    def returnLstat(self, pathSplit):
+        if pathSplit[len(pathSplit)-1].strip() == '': 
+                return os.lstat(self.src + pathSplit[0])
+        else:
+            p = self.src + pathSplit[0] + pathSplit[len(pathSplit)-1]
+            if os.path.isfile(p):
+                return os.lstat(p)
+            else:
+                pass
+    
+    def getResizeCommand(self, pathSplit, path):
+        pth = self.src + pathSplit[0] + pathSplit[len(pathSplit)-1]
+        command = "convert " +pth+ " -resize " +self.THUMB_width+ "x" +self.THUMB_height+ " " +self.pathTmpDir +path
+        return command
     
 def main():
     usage="""
