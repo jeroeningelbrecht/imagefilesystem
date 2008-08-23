e705716fd0fdd2a747bf1434cc5a4709727caa94
jeroeningelbrecht <jeroeningelbrecht@a888fc79-3454-0410-b17d-bf46f5c825f5>
2008-08-23 17:06:23 +0000

    version where all the wanted commands are handled. The user can give a command for himself. He may not forget to use the '%src' and the '%dest' parameters because otherwise, the command won't work.

--- MyImageMagickCommandMaker.py
+++ MyImageMagickCommandMaker.py
@@ -17,12 +17,25 @@ class MyImageMagickCommandMaker(CommandMaker):
         self.width = dict['width']
         self.height = dict['height']
         self.angle = dict['angle']
+        self.borderColor = dict['borderColor']
+        self.borderWidth = dict['borderWidth']
+        self.borderHeight = dict['borderHeight']
+        self.cropWidth = dict['cropWidth']
+        self.cropHeight = dict['cropHeight']
+        self.cropX = dict['cropX']
+        self.cropY = dict['cropY']
+        self.imageMagickCommand = dict['imageMagickCommand']
         
-        self.command = "convert \"" +self.srcPath+ "\""
-        self.resize = self.getResizeString()
-        
-        self.rotate = self.getRotationString()
-        self.command += self.resize + self.rotate+ " \""+self.destPath+ "\""
+        if self.imageMagickCommand != '':
+            toSplit = self.imageMagickCommand
+            self.command = toSplit.split("%src")[0]+self.srcPath+toSplit.split("%src")[1].split("%dest")[0]+self.destPath
+        else:
+            self.command = "convert \"" +self.srcPath+ "\""
+            self.resize = self.getResizeString()
+            self.rotate = self.getRotationString()
+            self.crop = self.getCropString()
+            self.border = self.getBorderString()
+            self.command += self.crop + self.resize + self.rotate + self.border + " \""+self.destPath+ "\""
         
     def executeCommand(self):
         os.popen(self.command)
@@ -46,3 +59,43 @@ class MyImageMagickCommandMaker(CommandMaker):
         else:
             self.rotate = " "
         return self.rotate
+    
+    def getBorderString(self):
+        string = ' '
+        if self.borderColor != '':
+            string += ' -bordercolor ' +self.borderColor
+    
+        if self.borderWidth != '' or self.borderHeight != '':
+            string += ' -border '
+            if self.borderWidth == "":
+                string += "x" + self.borderHeight
+            elif self.borderHeight == "":
+                string += self.borderWidth + "x"
+            else:
+                string += self.borderWidth + "x" + self.borderHeight + " "
+        return string
+    
+    def getCropString(self):
+        string = ' '
+        bool = 0
+        if self.cropWidth != '' or self.cropHeight != '':
+            string += ' -crop '
+            bool = 1
+            if self.cropWidth == "":
+                string += "x" + self.cropHeight
+            elif self.cropHeight == "":
+                string += self.cropWidth + "x"
+            else:
+                string += self.cropWidth + "x" + self.cropHeight
+        
+        if self.cropX != '' or self.cropY != '':
+            if not bool:
+                string += ' -crop '
+            if self.cropX == "":
+                string += self.cropY
+            elif self.cropY == "":
+                string += self.cropX
+            else:
+                string += self.cropX + self.cropY + " "
+        
+        return string
