69bbccbc9cf9992655dbc5b22326b11b90fab156
jeroeningelbrecht <jeroeningelbrecht@a888fc79-3454-0410-b17d-bf46f5c825f5>
2010-02-18 23:21:19 +0000

    Added some comment to ArgumentHandler.py

--- trunk/src/ArgumentHandler.py
+++ trunk/src/ArgumentHandler.py
@@ -17,7 +17,7 @@ class ArgumentHandler(object):
        
     def handleArguments(self, argv):
         try:
-            opts, args = getopt(argv, "hc:d:s:", ["help", "config=","destination=","source="])
+            opts, args = getopt(argv, "hc:d:m:s:", ["help", "config=","destination=","mountpoint=","source="])
         except GetoptError:
             self.usage()
             sys.exit(2)
@@ -27,7 +27,7 @@ class ArgumentHandler(object):
                 sys.exit()
             elif opt in ("-c","--config"):
                 self.configFile = os.path.abspath(arg)
-            elif opt in ("-d","--destination"):
+            elif opt in ("-d","--destination","-m","--mountpoint"):
                 self.destination = os.path.abspath(arg) #destination folder
                 #create the destination folder if it does not exist yet
                 if not os.path.isdir(self.destination):
@@ -35,15 +35,24 @@ class ArgumentHandler(object):
             elif opt in ("-s", "--source"):
                 self.source = os.path.abspath(arg) #sourcefolder
             else: self.usage()
-            
+                 
     def getDestination(self):
+        """ 
+        Get the destination path. This is the same as the mountpoint. Note that, if if didn't exist yet, it's created during the initialisation
+        """   
         return self.destination
     
     def getSource(self):
+        """
+        Get the source path. This is the folder which contains the original images.
+        """
         return self.source
     
     def getConfigFile(self):
+        """
+        Get the path of the configuration file.
+        """
         return self.configFile
     
     def usage(self):
-        print "python PATH_TO_imagefs.py [sourcefolder] [destinationfolder] [-c/--config PATH_TO_configurationXML file]"
+        print "python PATH_TO_imagefs.py -s/--source [sourcefolder] -d/--destination/-m/--mountpoint[destinationfolder/mountpoint] [-c/--config PATH_TO_configurationXML file]"
